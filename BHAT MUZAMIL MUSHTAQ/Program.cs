30121b79704875c7a993b3dd1e6657baf94fbbb0
MUZAMILMUSHTAQ <muzamimushtaq1313@gmail.com>
2024-03-16 11:51:02 +0530

    test

--- BHAT MUZAMIL MUSHTAQ/Program.cs
+++ BHAT MUZAMIL MUSHTAQ/Program.cs
@@ -6,7 +6,15 @@
         {
             Console.WriteLine("Hello, World!");
 
-            
+            int a =20;
+            int b = 30;
+            int c;
+            c = a + b;
+            Console.WriteLine(c);
+            for(int i=0;i<50;i++)
+            {
+                Console.WriteLine( i);
+            }
         }
     }
 }
